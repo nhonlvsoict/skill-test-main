ecf2fc5c2554aaee219250f1fb77187458adf77d
nhonlv <nhonlv.soict@gmail.com>
2026-01-20 01:33:41 +0000

    add request validation for student API

--- backend/src/modules/students/sudents-router.js
+++ backend/src/modules/students/sudents-router.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const studentController = require("./students-controller");
+const { validateRequest } = require("../../utils/validate-request");
+const { 
+    addStudentSchema, 
+    updateStudentSchema, 
+    updateStudentStatusSchema,
+    getStudentDetailSchema 
+} = require("./students-validation");
 
 router.get("", studentController.handleGetAllStudents);
-router.post("", studentController.handleAddStudent);
-router.get("/:id", studentController.handleGetStudentDetail);
-router.post("/:id/status", studentController.handleStudentStatus);
-router.put("/:id", studentController.handleUpdateStudent);
+router.post("", validateRequest(addStudentSchema), studentController.handleAddStudent);
+router.get("/:id", validateRequest(getStudentDetailSchema), studentController.handleGetStudentDetail);
+router.post("/:id/status", validateRequest(updateStudentStatusSchema), studentController.handleStudentStatus);
+router.put("/:id", validateRequest(updateStudentSchema), studentController.handleUpdateStudent);
 
 module.exports = { studentsRoutes: router };
